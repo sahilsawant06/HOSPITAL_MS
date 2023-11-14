2d8f6f3a36e35910225e0f7d9056a04771e17b3c
sahil <sawant.sahil@fyntune.com>
2023-11-14 19:06:56 +0530

    14-11-23 PFkey

---
+++ database/migrations/2023_11_14_124950_create_libraries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libraries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fid');
+            $table->foreign('fid')->references('id')->on('primforien');
+            $table->string('books');
+            $table->date('due_date')->nullable();
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libraries');
+    }
+};
